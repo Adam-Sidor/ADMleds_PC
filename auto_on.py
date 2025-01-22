5f139fea1975ffc75fee77a4e07ff9c1390191ae
Adam Sidor <sidoadsi1@gmail.com>
2025-01-22 22:24:24 +0100

    Getting IPs from file

--- auto_on.py
+++ auto_on.py
@@ -1,13 +1,30 @@
 import requests
+import re
 
 def checkInternetStatus():
     try:
-        response = requests.get("http://google.com",timeout=5)
+        requests.get("http://google.com",timeout=5)
         return True
     except requests.ConnectionError:
         return False
 
+def readIPs(filePath):
+    try:
+        with open(filePath,'r') as file:
+            content = file.read()
+            pattern = r"IP:\s*\[\s*([^\]]+)\s*\]"
+            match = re.search(pattern, content)
+            if match:
+                ipAddresses = match.group(1)
+                ipList = [ip.strip() for ip in ipAddresses.split(";")]
+                return ipList
+    except FileNotFoundError:
+        print("File not found!")
+        return []
+
 if checkInternetStatus():
     print("Masz internet")
+    IPs = readIPs("config.txt")
+    print(IPs)
 else:
     print("brak")
